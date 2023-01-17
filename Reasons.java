15bcfefefb52f79f727d42ae500eb9f545eff0e9
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-01-17 11:53:15 +0530

    Create Reasons.java

---
+++ Reasons.java
@@ -0,0 +1,14 @@
+Why Jvm Architecture?
+//JVM in Java is the engine that drives the Java Code. It converts Java bytecode into machines language.
+//JVM architecture in Java contains classloader, memory area, execution engine etc.
+//In JVM, Java code is compiled to bytecode.
+//Also performs the task of Garbage Collection.
+
+
+Why java platform independant?
+//Platform=Microprocessor + OS(Intel +Windows)
+//Mll=Platform Dependant(will only work in the platform it was compiled)
+//Platform independent(hll->java compiler->Bytecode(secured and which is platform independant)->JVM(platform dependant)
+//JDK=JRE+JVM(of that particular platform)
+  
+  
