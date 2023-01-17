6dce861cd7ede67cddf0f833d23984067e49c165
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-01-17 12:51:28 +0530

    Update Reasons.java

--- Reasons.java
+++ Reasons.java
@@ -12,3 +12,73 @@ Why java platform independant?
 //JDK=JRE+JVM(of that particular platform)
   
   
+Garbage Collection in Stack?
+//Stack is last in first out so there is no need to garbage collect.
+  
+Stack vs Heap?
+//Stack-Method invocations and Local variables live here(LIFO)-Object variables lives on the stack 
+//Heap-Objects that would be deleted soon lives here-Object lives on Heap
+  
+Where is modifiers used in java?
+//Access data to other files
+//1.Default=Everyone can see it no restrictions(SPECIFIC PACKAGE)
+//2.Public=Same as Default (ANYWHERE)
+//3.Private=Privated to one class(SPECIFIC CLASS)
+//4.Protected=Same package access and can be accessed by other packages with the help of "extends"(SUBSIDING CLASS)
+
+  
+Example of these modifiers?
+//Default=Package the name of the employee at zoho people
+//Public=ur name can be used and can be searched anywhere 
+//Privated=ur personal details of your salary is hidden and only can be viewed to one who manages your salary account 
+//Protected=your phone number can be accessed by your manager or reporting to member.
+  
+Where can static class/variables /methods can be used?
+1.What is static?
+  use global variables inside other classes without the need of creating an object and then refrencing it.
+ example:
+a)The error:
+class Calender{
+    int month=12;//global variable
+    public static void main(String[] args) {
+        int year=2022;
+        System.out.println(year);
+        System.out.println(month);/*error as non-static variable month cannot be referenced from a static context*/
+    }
+}
+b)how to rectify:
+class Calender{
+    int month=12;//global variable
+    public static void main(String[] args) {
+        int year=2022;
+        Calender cal=new Calender();//to remove the error we need to create and object and then reference it
+        System.out.println(year );
+        System.out.println("\n"+cal.month);
+    }
+}
+c)Instead use Static keyword to globally access it:
+class Calender{
+    static int month=12;//global variable
+    public static void main(String[] args) {
+        int year=2022;
+        System.out.println(year);
+        System.out.println(month);
+    }
+}
+
+2.What is a Static class and where can it be used ?  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
