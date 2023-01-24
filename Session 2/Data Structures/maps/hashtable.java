8c9e1a46df7a9f74ea8cb3b02cbad655f3f2b36a
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-01-24 15:33:52 +0530

    Create hashtable.java

---
+++ Session 2/Data Structures/maps/hashtable.java
@@ -0,0 +1,9 @@
+/*Difference between hashmap and hashtable:
+data is same 
+internally follows a hashcode 
+Synchronized thread meaning the thread is safe 
+one thread allowed at a time 
+performance is poor
+no nulls will be acceopted 
+new obj capacity is 11 and load factor =0.75 
+key|value|hash|next */
