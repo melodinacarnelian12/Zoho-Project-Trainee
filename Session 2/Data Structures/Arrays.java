c2dae70ccb82d7f557b465ba50488a5b1358eeb1
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-01-24 15:26:57 +0530

    Create Arrays.java

---
+++ Session 2/Data Structures/Arrays.java
@@ -0,0 +1,13 @@
+class Main {
+ public static void main(String[] args) 
+   int[] me = {12, 4, 5, 2, 5};
+
+   // access each array elements
+   System.out.println("Accessing Elements of Array:");
+   System.out.println("First Element: " + me[0]);
+   System.out.println("Second Element: " + me[1]);
+   System.out.println("Third Element: " + me[2]);
+   System.out.println("Fourth Element: " + me[3]);
+   System.out.println("Fifth Element: " + me[4]);
+ }
+}
