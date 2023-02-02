37e80501df963c4d0592309aa1a519bde955e319
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-02-02 16:57:13 +0530

    Create Agregation.java

---
+++ Reasons3/Agregation.java
@@ -0,0 +1,38 @@
+//Has a relationship
+//But both class can exist without the other class 
+//An author can exist without a book 
+//And a book can Exist without the author 
+//Advantages reusability of codes!
+
+class Author
+{
+  String authorName;
+  int age;
+  String place;
+  Author(String name, int age, String place)
+  {
+    this.authorName = name;
+    this.age = age;
+    this.place = place;
+  }
+}
+
+class  Book
+{
+  String name;
+  int price;
+  Author auther;//using the other class 
+  Book(String n, int p, Author auther)
+  {
+    this.name = n;
+    this.price = p;
+    this.auther = auther;
+    }
+  
+}
+
+
+//Aggregation:
+When you want to use some property or behaviour of any class
+without the requirement of modifying it or adding more functionality to it, in such case Aggregation.
+But if you want to modify the parameters on the code later then use inheritance
