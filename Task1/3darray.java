900caa4912cc29144eb6734b45f9a70aed87fa13
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-02-06 14:45:40 +0530

    Update 3darray.java

--- Task1/3darray.java
+++ Task1/3darray.java
@@ -46,3 +46,77 @@ public static int[][] getArray() {
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+or 
+
+
+
+
+
+
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Sudo1 {
+    public static void removeDuplicates(int[][][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    for (int l = 0; l < array.length; l++) {
+                        for (int m = 0; m < array[l].length; m++) {
+                            for (int n = 0; n < array[l][m].length; n++) {
+                                if (array[i][j][k] == array[l][m][n] && i != l && j != m && k != n) {
+                                    array[l][m][n] = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static int[][][] getArray() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = input.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int columns = input.nextInt();
+        System.out.print("Enter the number of depth: ");
+        int depth = input.nextInt();
+        int[][][] array = new int[rows][columns][depth];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    System.out.print("Enter the value of array[" + i + "][" + j + "][" + k + "]: ");
+                    array[i][j][k] = input.nextInt();
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[][][] array = getArray();
+        removeDuplicates(array);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    System.out.print(array[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
