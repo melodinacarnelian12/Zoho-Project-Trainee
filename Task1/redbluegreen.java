82f410a594912c89dc3f925b70b6b6c3f8d7d5b9
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-02-06 11:02:49 +0530

    Update redbluegreen.java

--- Task1/redbluegreen.java
+++ Task1/redbluegreen.java
@@ -1,28 +1,28 @@
-
 import java.util.Scanner;
-class Stringcombine 
+class Stringcombine
 {
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        String ch=sc.next();
-        while(str){
-         for(int i=0; i<1; i++)
-           {
-              int asciiValue1 = str.charAt(i);
-              
-               for(int j=i; j<str.length(); j++)
-                   {
-                      int asciiValuen = str.charAt(j);
-                      if(asciiValue1==asciiValuen)
-                      {
-                         System.out.println(str.charAt(j));
-                      }
-                      
-                   }str=str.replace(ch,"");
-                   System.out.println(str);
-           }    
-          }
-         }
+        while(str != null && !str.isEmpty())
+        {
+            for (int i = 0; i < 1; i++) 
+            {
+                int asciiValue1 = str.charAt(i);
+                for (int j = i; j < str.length(); j++) 
+                {
+                    int asciiValuen = str.charAt(j);
+                    if (asciiValue1 == asciiValuen)
+                    {
+                        str = str.replaceAll("\n", "");
+                        System.out.println(str.charAt(j));
+                    }
+                }
+                char k = (char) asciiValue1;
+                String h = String.valueOf(k);
+                str = str.replace(h, "");
+            }
+        }
     }
+}
