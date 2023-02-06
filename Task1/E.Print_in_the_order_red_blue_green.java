6de6bf921309196483adf0703c6afe3ee9908979
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-02-06 17:31:28 +0530

    Update and rename redbluegreen.java to E.Print_in_the_order_red_blue_green.java

--- Task1/E.Print_in_the_order_red_blue_green.java
+++ Task1/E.Print_in_the_order_red_blue_green.java
@@ -1,3 +1,13 @@
+5.Given an mixed array of Red, Blue and Green colour balloons, arrange all the same colour balloons together by maintaining the order of colours based on first occurrence in the input array.
+Input: arr[] = {R, G, B, G, B, G, R}
+Output:{R, R, G, G, G, B, B}
+
+Input: arr[] = {B, G, B, R, G, R, B, G, R, R, B}
+Output: {B, B, B, B, G, G, G, R, R, R, R}
+
+
+
+Approach A:
 import java.util.Scanner;
 class Stringcombine
 {
@@ -7,16 +17,16 @@ class Stringcombine
         String str = sc.nextLine();
         while(str != null && !str.isEmpty())
         {
-            for (int i = 0; i < 1; i++) 
+            for (int i = 0; i < 1; i++) //get the first element 
             {
-                int asciiValue1 = str.charAt(i);
-                for (int j = i; j < str.length(); j++) 
+                int asciiValue1 = str.charAt(i);//get the ascii value of the first element
+                for (int j = i; j < str.length(); j++) //compare with other elements of the array 
                 {
-                    int asciiValuen = str.charAt(j);
-                    if (asciiValue1 == asciiValuen)
+                    int asciiValuen = str.charAt(j);//get the ascii value of it 
+                    if (asciiValue1 == asciiValuen)//if both asciis are equal
                     {
                         str = str.replaceAll("\n", "");
-                        System.out.println(str.charAt(j));
+                        System.out.println(str.charAt(j));//print it 
                     }
                 }
                 char k = (char) asciiValue1;
@@ -33,7 +43,8 @@ class Stringcombine
 or
 
 
-
+Approach B:
+//USING arrays and printing in order 
 
 import java.util.Scanner;
 import java.util.Arrays;
