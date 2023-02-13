548d5de1f60b89e8662d8b8073ad71afd60af44c
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-02-13 10:43:38 +0530

    Create Single Responsibility Principle.java

---
+++ Task1/Solid/Single Responsibility Principle.java
@@ -0,0 +1,22 @@
+Single Responsibility Principle (SRP):
+A class should have only one reason to change. Each class should have a single responsibility.
+
+class BankAccount {
+    private double balance;
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
