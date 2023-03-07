634e66555f1735ed495bbd67f20b9a2de0a6eca2
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-03-07 22:18:59 +0530

    Create abstractclass.java

---
+++ Oops/abstraction/abstractclass.java
@@ -0,0 +1,67 @@
+public abstract class BankAccount {
+    private double balance;
+    private String accountNumber;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public abstract void calculateInterest();
+}
+
+
+
+
+public class SavingsAccount extends BankAccount {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public void calculateInterest() {
+        double interest = getBalance() * interestRate;
+        deposit(interest);
+    }
+}
+
+public class CheckingAccount extends BankAccount {
+    private double overdraftLimit;
+
+    public CheckingAccount(String accountNumber, double balance, double overdraftLimit) {
+        super(accountNumber, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (getBalance() - amount >= -overdraftLimit) {
+            super.withdraw(amount);
+        } else {
+            System.out.println("Withdrawal denied: insufficient funds.");
+        }
+    }
+
+    @Override
+    public void calculateInterest() {
+    }
+}
