045e2c2a26336f02ec96688759333cc112c803c3
melodinacarnelian12 <122588717+melodinacarnelian12@users.noreply.github.com>
2023-03-07 22:40:24 +0530

    Create single inheritance.java

---
+++ Oops/inheritance/single inheritance.java
@@ -0,0 +1,36 @@
+// BankAccount class representing a basic bank account
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class SavingsAccount extends BankAccount {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void addInterest() {
+        double interest = getBalance() * interestRate;
+        deposit(interest);
+    }
+}
